9dec3de5fbd7f952a2ce5756ff989168206e3006
knoxkeaton <knoxandlolo@gmail.com>
2017-10-10 18:25:34 -0400

    Lab

--- test/com/test1/calculator/CalculatorTest.java
+++ test/com/test1/calculator/CalculatorTest.java
@@ -1,47 +1,39 @@
-package com.lab1.calculator;
+package com.test1.calculator;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.lab1.calculator.Calculator;
+
 public class CalculatorTest {
-	
 	private Calculator calculator;
 	
 	@Before
 	public void setUp() {
 		calculator = new Calculator();
 	}
-
 	@Test
 	public void addTest() {
 		assertEquals(4, calculator.add(2, 2));
 	}
-	
 	@Test
 	public void subtractTest() {
-		assertEquals(4, calculator.subtract(8, 4));
+		assertEquals(4, calculator.subtract(6, 2));
 	}
-	
 	@Test
 	public void multiplyTest() {
 		assertEquals(4, calculator.multiply(2, 2));
 	}
-	
 	@Test
 	public void divideTest() {
 		assertEquals(4, calculator.divide(8, 2));
 	}
-	
 	@Test
-	public void isEqualPositiveTest() {
-		assertTrue(calculator.isEqual(2, 2));
-	}
-	
-	@Test
-	public void isEqualNegativeTest() {
-		assertFalse(calculator.isEqual(4, 2));
+	public void equalTest() {
+		assertTrue(calculator.isEqual(8, 8));
+		assertFalse(calculator.isEqual(8, 27));
 	}
 
-}
+} 
